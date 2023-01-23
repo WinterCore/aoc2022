5e1fcb5c108fc96f97b202ca6add61f1dc7a000b
WinterCore <hogobbl@gmail.com>
2023-01-23 19:22:22 +0200

    chore: Refactor day 17 part 1

--- day17/main.rs
+++ day17/main.rs
@@ -1,7 +1,7 @@
-use std::{fs, collections::HashSet, char::CharTryFromError, thread::sleep, time::Duration, cmp};
+use std::{fs, collections::{HashSet, HashMap}, char::CharTryFromError, thread::sleep, time::{Duration, Instant}, cmp};
 use ncurses::*;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 struct Point(i32, i32);
 
 
@@ -18,7 +18,7 @@ impl RockCluster {
             // Plus sign
             Self { points: vec![Point(1, 0), Point(1, 1), Point(0, 1), Point(2, 1), Point(1, 2)] },
             // Reversed L
-            Self { points: vec![Point(2, 0), Point(2, 1), Point(2, 2), Point(1, 2), Point(0, 2)] }, 
+            Self { points: vec![Point(2, 0), Point(2, 1), Point(2, 2), Point(1, 0), Point(0, 0)] }, 
             // Vertical Line
             Self { points: vec![Point(0, 0), Point(0, 1), Point(0, 2), Point(0, 3)] },
             // Cube
@@ -48,9 +48,10 @@ impl RockCluster {
             .any(|tp| tp.0 == p.0 && tp.1 == p.1)
     }
 }
+
+
 #[derive(Debug, PartialEq, Eq)]
 enum Direction {
-    Top,
     Right,
     Down,
     Left,
@@ -59,97 +60,85 @@ enum Direction {
 impl Direction {
     fn to_move_delta(&self) -> Point {
         match self {
-            Direction::Top => Point(0, -1),
             Direction::Right => Point(1, 0),
-            Direction::Down => Point(0, 1),
+            Direction::Down => Point(0, -1),
             Direction::Left => Point(-1, 0),
         }
     }
 }
 
 #[derive(Debug)]
-struct RegionChamber {
-    // https://gamedev.stackexchange.com/a/76209
-    zone_rock_clusters: Vec<HashSet<usize>>,
-    zone_size: u32,
+struct Chamber {
+    rested: HashMap<i32, HashSet<Point>>,
     width: u32,
-    max_height: u32,
-
-    rested_rock_clusters: Vec<RockCluster>,
+    height_reached: u32,
 }
 
-impl RegionChamber {
-    fn get_point_zone(&self, Point(x, y): &Point) -> usize {
-        let zones_per_width = (self.width as f32 / self.zone_size as f32).ceil() as usize;
-        let zone = (y * zones_per_width as i32) + (x / self.zone_size as i32);
+use std::sync::atomic::{AtomicUsize, Ordering};
 
-        zone as usize
-    }
+static MS: AtomicUsize = AtomicUsize::new(0);
 
-    fn is_colliding_with_zone(&self, rock: &RockCluster, zone: usize) -> bool {
-        if let None = self.zone_rock_clusters.get(zone) {
-            return false;
+
+impl Chamber {
+    fn move_rock_dir(&self, rock: &RockCluster, dir: &Direction) -> Option<RockCluster> {
+        let new_rock = rock.move_by(dir.to_move_delta());
+        
+        // let is_colliding_with_rested_rocks = points.iter().any(|p| new_rock.is_colliding_with_point(p));
+        let mut is_colliding_with_rested_rocks = false;
+        
+        for point in new_rock.points.iter() {
+            let row = self.rested.get(&point.1);
+
+            match row {
+                Some(set) => {
+                    if set.contains(&point) {
+                        is_colliding_with_rested_rocks = true;
+                        break;
+                    }
+                },
+                _ => (),
+            }
         }
 
-        self.zone_rock_clusters[zone]
-            .iter()
-            .filter_map(|&i| self.rested_rock_clusters.get(i))
-            .any(|rc| rc.is_colliding_with_rock(rock))
-    }
+        let is_colliding_with_walls = new_rock.points.iter().any(|p| p.0 < 0 || (self.width as i32) <= p.0);
+        let is_colliding_with_floor = new_rock.points.iter().any(|p| p.1 < 0);
 
-    fn is_rock_colliding(&self, rock: &RockCluster) -> bool {
-        let is_colliding_with_floor = rock.points
-            .iter()
-            .any(|p| p.1 >= self.max_height as i32);
+        // println!("{:?}\n{:?}\n{:?}\n\n", rows, points, self.rested);
 
-        let is_colliding_with_walls = rock.points
-            .iter()
-            .any(|p| p.0 < 0 || (self.width as i32) <= p.0);
-
-        // Walls collision
-        if is_colliding_with_walls || is_colliding_with_floor {
-            return true;
+        // println!("\n");
+        // println!("{:?} {:?} {:?} {:?}", new_rock, is_colliding_with_floor, is_colliding_with_walls, is_colliding_with_rested_rocks);
+        if is_colliding_with_floor || is_colliding_with_walls || is_colliding_with_rested_rocks {
+            return None;
         }
 
-        let covered_zones: HashSet<usize> = rock.points
-          .iter()
-          .fold(HashSet::new(), |mut hs, p|  {
-              hs.insert(self.get_point_zone(p));
-              hs
-          });
-
-        // Optimize by only checking for zones that exist in provided direction
-        covered_zones
-            .iter()
-            .any(|&z| self.is_colliding_with_zone(rock, z))
+        Some(new_rock)
     }
 
     fn rest_rock_cluster(&mut self, rock: RockCluster) {
-        let idx = self.rested_rock_clusters.len(); 
+        self.height_reached = self.height_reached.max(
+            rock.points
+                .iter()
+                .map(|x| x.1 + 1)
+                .max()
+                .unwrap_or(0) as u32
+        );
 
         rock
             .points
             .iter()
             .for_each(|p| {
-                let zone = self.get_point_zone(p);
-                
-                if let Some(clusters) = self.zone_rock_clusters.get_mut(zone) {
-                    clusters.insert(idx);
-                }
+                let set = self.rested.get_mut(&p.1);
+
+                match set {
+                    Some(data) => {
+                        data.insert(*p);
+                    },
+                    None => {
+                        self.rested.insert(p.1, HashSet::from_iter([*p]));
+                    },
+                };
             });
 
-        self.rested_rock_clusters.push(rock);
-    }
-
-    fn move_rock_dir(&self, rock: &RockCluster, dir: &Direction) -> Option<RockCluster> {
-        let new_rock = rock.move_by(dir.to_move_delta());
-        let is_colliding = self.is_rock_colliding(&new_rock);
-
-        if is_colliding {
-            return None;
-        }
-
-        Some(new_rock)
     }
 
     fn simulate_rock_fall(
@@ -168,12 +157,15 @@ impl RegionChamber {
         loop {
             let jet = &gas_jets[*gas_jet_idx % gas_jets.len()];
             let dir = if down { &Direction::Down } else { jet };
+
             let moved_rock = self.move_rock_dir(&rock, dir);
+            // println!("{:?}", instant.elapsed().as_micros());
             
             if render {
-                draw(self, gas_jets, *gas_jet_idx, &rock);
+                draw(self, gas_jets, *gas_jet_idx % gas_jets.len(), &rock);
             }
             // println!("{:?} {:?}", dir, moved_rock.as_ref().unwrap_or(&rock));
+            
 
 
             if down {
@@ -182,6 +174,7 @@ impl RegionChamber {
                         rock = new_rock;
                     },
                     None => {
+                        // println!("Resting {:?}", rock);
                         self.rest_rock_cluster(rock.clone());
                         break;
                     },
@@ -194,6 +187,9 @@ impl RegionChamber {
                 }
             }
 
+            getch();
+            // sleep(Duration::from_millis(100));
+            // sleep(Duration::from_millis(200));
             down = ! down;
         }
 
@@ -215,35 +211,11 @@ fn parse(input: &str) -> Vec<Direction> {
         })
         .collect::<Vec<Direction>>();
 
-
-    /*
-    let mut grouped: Vec<GasJet> = vec![];
-
-    for curr in list.into_iter() {
-        let last = grouped.last_mut();
-
-        match last {
-            Some(last) => {
-                if std::mem::discriminant(last) == std::mem::discriminant(&curr) {
-                    *last = last.add(curr.val());
-                } else {
-                    grouped.push(curr);
-                }
-            },
-            None => {
-                grouped.push(curr);
-            }
-        }
-    }
-
-    grouped
-    */
-
     list
 }
 
 fn draw(
-    chamber: &RegionChamber,
+    chamber: &Chamber,
     gas_jets: &Vec<Direction>,
     gas_jet_idx: usize,
     falling_rock: &RockCluster,
@@ -252,15 +224,17 @@ fn draw(
 
     let height = LINES() - 1;
     let width = COLS();
-    let chamber_draw_width = (chamber.width + 2) as i32;
+    let chamber_draw_width = (chamber.width + 1) as i32;
 
-    let shift_x = 2;
-    let raw_shift_y = falling_rock.points.iter().max_by_key(|x| x.1).unwrap().1 * -1 + height / 2;
+    let min_y = chamber.rested.keys().min().unwrap_or(&0);
+    let max_y = chamber.rested.keys().max().unwrap_or(&0);
+
+    let shift_x = 1;
+    // let raw_shift_y = falling_rock.points.iter().max_by_key(|x| x.1).unwrap().1 * -1 + height / 2;
     let shift_y = cmp::max(
-        raw_shift_y,
-        (chamber.max_height as i32 - height) * -1,
+        (height - 1) + (max_y - min_y) - height / 2,
+        height - 1,
     );
-        
 
     // Draw walls
     for i in 0..height {
@@ -278,21 +252,22 @@ fn draw(
     }
 
     // Draw rested rocks 
-    for rested_rock in chamber.rested_rock_clusters.iter() {
-        for Point(x, y) in rested_rock.points.iter() {
-            mvaddch(y + shift_y, x + shift_x, '#' as u32);
+    for points in chamber.rested.values() {
+        for Point(x, y) in points.iter() {
+            mvaddch((y * -1) + shift_y, x + shift_x, '#' as u32);
         }
     }
 
 
     // Draw falling rock
     for Point(x, y) in falling_rock.points.iter() {
-        mvaddch(y + shift_y, x + shift_x, '@' as u32);
+        mvaddch((y * -1) + shift_y, x + shift_x, '@' as u32);
     }
 
 
     let jets_per_row = width - chamber_draw_width - 5;
     let jets_left_padding = 2;
+    let jets_top_padding = 2;
 
     let get_jet_char = |jet: &Direction| {
         match jet {
@@ -302,10 +277,13 @@ fn draw(
         }
     };
 
+    // Draw reached height
+    mvaddstr(0, chamber_draw_width + jets_left_padding, &format!("Reached height: {}", chamber.height_reached));
+
     // Draw Jets
     for (i, jet) in gas_jets.iter().enumerate() {
 
-        let y = i as i32 / jets_per_row;
+        let y = i as i32 / jets_per_row + jets_top_padding;
         let x = i as i32 % jets_per_row + jets_left_padding + chamber_draw_width;
 
         if i == gas_jet_idx {
@@ -319,7 +297,7 @@ fn draw(
 
     refresh();
 
-    sleep(Duration::from_millis(50));
+    //sleep(Duration::from_millis(15));
 }
 
 fn main() {
@@ -333,63 +311,51 @@ fn main() {
 }
 
 fn part1(gas_jets: &Vec<Direction>, render: bool) -> String {
-    let rock_clusters = RockCluster::get_rock_clusters();
-    let width = 7;
-    let max_height = 10000;
-    let zone_size = 4;
-    let zone_count: usize = (width as f32 / zone_size as f32).ceil() as usize * max_height as usize;
-    // println!("Zone count: {}", zone_count);
-
     if render {
         initscr();
         use_default_colors();
         start_color();
-        init_pair(1, COLOR_RED, -1);
+        init_pair(1, COLOR_BLACK, COLOR_GREEN);
     }
 
-    let mut lowest_y = max_height as i32;
-    let mut chamber = RegionChamber {
-        rested_rock_clusters: Vec::new(),
-        zone_rock_clusters: vec![HashSet::new(); zone_count],
-        zone_size,
-        width,
-        max_height,
-    };
-
+    let instant = Instant::now();
+    let rock_clusters = RockCluster::get_rock_clusters();
     let mut gas_jet_idx = 0;
+
+    let mut chamber = Chamber {
+        rested: HashMap::new(),
+        width: 7,
+        height_reached: 0,
+    };
 
     for rock_num in 0..2022 {
         let raw_rock_cluster = rock_clusters[rock_num % rock_clusters.len()].clone();
-        let raw_rock_cluster_bottom = raw_rock_cluster
-            .points
-            .iter()
-            .max_by_key(|x| x.1)
-            .unwrap()
-            .1;
 
         let rock_cluster = raw_rock_cluster
-            .move_by(Point(2, lowest_y - (4 + raw_rock_cluster_bottom)));
+            .move_by(Point(2, chamber.height_reached as i32 + 3));
 
-        let rested_rock = chamber.simulate_rock_fall(
+        chamber.simulate_rock_fall(
             gas_jets,
             &mut gas_jet_idx,
             &rock_cluster,
             render,
         );
 
-        rested_rock.points.iter().for_each(|p| {
-            lowest_y = lowest_y.min(p.1);
-        });
+        if rock_num % rock_clusters.len() == 0 && gas_jet_idx % gas_jets.len() == 0 {
+            println!("{}", chamber.height_reached);
+        }
     }
+
+    MS.fetch_add(instant.elapsed().as_micros() as usize, Ordering::SeqCst);
+    println!("{:?}", MS.load(Ordering::Relaxed) / 1000);
 
     if render {
         getch();
         endwin();
     }
 
-    // println!("{:?}", chamber);
-    println!("{:?}", max_height as i32 - lowest_y);
-    String::from("")
+
+    String::from(chamber.height_reached.to_string())
 }
 
 
